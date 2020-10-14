d7eece696d96f7be60939fdf07c0a81e71e2ed5c
ThalesChamone <72635068+ThalesChamone@users.noreply.github.com>
2020-10-14 08:31:37 -0300

    Add files via upload

---
+++ Exercicio7WHILE.c
@@ -0,0 +1,34 @@
+//Fazer um algoritmo que calcule e exiba na tela o fatorial de um número
+//fornecido pelo usuário, usando o comando While.Repetir a execução do
+//algoritmo tantas vezes quantas o usuário quiser.Lembre - se que o resultado
+//do cálculo de um fatorial pode ser um número “grande”
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <locale.h>
+
+int i;
+double n, n1, fat;
+
+int main() {
+
+	while (n >= 0) {
+
+		printf("\n\nDigite um numero para saber seu fatorial: ");
+		scanf_s("%lf", &n1);
+
+		for (fat = 1; n1 > 1; n1--)
+		{
+			fat = fat * n1;
+
+			printf("\nFatorial calculado: %.2lf", fat);
+		}
+
+	}
+
+	
+}
